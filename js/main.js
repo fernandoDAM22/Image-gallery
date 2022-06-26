dab8fc1ed19e870d074d40cd6a19749d09ea027a
Fernando Gil <fernandoesmr@gmail.com>
2022-06-26 16:56:49 +0200

    completed project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*Variables*/
+const selectedImg = document.getElementById("selected-img");
+const images = document.querySelectorAll(".gallery-img");
+const box = document.getElementById("container");
+const btnNext = document.getElementById("next");
+const btnPrev = document.getElementById("previous");
+const btnClose = document.getElementById("close");
+let index = 0;
+/*Assing the eventListener to the images */
+images.forEach(element => {
+    element.addEventListener("click",open,false);
+});
+/*Assing the eventListener to the next and previus button */
+btnNext.addEventListener("click",next);
+btnPrev.addEventListener("click",previous);
+/*Assing the eventListener to the close button, in this case we use an anonymous function 
+because the operation is very simple */
+btnClose.addEventListener("click",()=>{
+    box.style.display = 'none';
+});
+/**This function allows us open the selected image */
+function open(e){
+    box.style.display = 'flex';
+    selectedImg.src = e.target.src;
+    index = Array.from(images).indexOf(e.target);
+}
+/**This function allows us go to the next image */
+function next(){
+    if(index === images.length - 1){
+        index = -1;
+    }
+    selectedImg.src = images[index + 1].src;
+    index++;
+}
+/**This function allows us go to the previous image */
+function previous(){
+    if(index === 0){
+        index = images.length;
+    } 
+    selectedImg.src = images[index - 1].src;
+    index--;
+}
